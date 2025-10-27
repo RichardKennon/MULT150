5328e2184cb02270b65e23043d78126f7c13af67
RichardKennon <rkennon@student.sdccd.edu>
2025-10-27 14:00:03 -0700

    Reached TIY

--- rkennon_Hour9/Assets/Scripts/ExerciseTriggerScript.cs
+++ rkennon_Hour9/Assets/Scripts/ExerciseTriggerScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerScript : MonoBehaviour
+public class ExerciseTriggerScript : MonoBehaviour
 {
     void OnTriggerEnter (Collider other)
     {

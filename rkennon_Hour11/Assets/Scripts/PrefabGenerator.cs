5ea4cf2fe4215cb8f01aaf0e0aad6a8cd7d79aef
RichardKennon <rkennon@student.sdccd.edu>
2025-11-11 16:44:21 -0800

    Hour11ACTUALLYdone

--- rkennon_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ rkennon_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -1,8 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    int BuiltLamps = 0;
+
+    void Start()
+    {
+        StreetLiner();
+        Debug.Log("There are " + BuiltLamps + " built lamps!");
+    }
 
     void Update()
     {
@@ -18,6 +27,17 @@ public class PrefabGenerator : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(prefab, transform.position, transform.rotation);
+        }
+
+    }
+
+    void StreetLiner()
+    {
+        for (int count = 1; count <= 10; count++)
+        {
+            BuiltLamps++;
+            Instantiate(prefab, transform.position, transform.rotation);
+            transform.Translate(10f, 0f, 0f);
         }
     }
 }

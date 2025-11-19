067060847e0c980eed403e9ace9582791693677a
RichardKennon <rkennon@student.sdccd.edu>
2025-11-18 19:04:10 -0800

    WeHaveLiftOff!

--- rkennon_Hour14and16/Assets/ParticleEffect.cs
+++ rkennon_Hour14and16/Assets/ParticleEffect.cs
@@ -1,7 +1,15 @@
 /*
  
+ Made a quick prefab to attach the particles to and a Launch Pad to push off against.
+
+ 1. I gave the particles collision set to Type: World with a Collider Force of 50.
  
- 
- 
- 
+ 2. I attempted to give the particles a lighting feature by adding a new lightsource and attaching it with a 0.6 ratio.
+
+-2.1 I also created a gray collored ground material in hopes of seeing a glow.
+
+ 4. I changed the renderer to a sphere mesh and scaled it down.
+
+ 5. I created a new material called RocketFlames and attached it to the renderer, making the spheres transparent, red, and somewhat shiny.
+
  */

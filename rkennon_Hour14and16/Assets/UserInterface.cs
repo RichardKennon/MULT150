c094fc99fb94434000b9fc3e00f06bfa64944039
RichardKennon <rkennon@student.sdccd.edu>
2025-11-18 18:13:22 -0800

    FinalStage?

---
+++ rkennon_Hour14and16/Assets/UserInterface.cs
@@ -0,0 +1,13 @@
+/*
+ First I changed the background to an image I saved in the past for a different assignment, downloaded off the internet.
+
+Second I changed the Title, Subtext, and Start Text to match the theme, turning it into the start screen of a no-n-existant sci-fi game.
+- The title became Protocal 0, the sub text a decoration for the theme, and the start button "Initiate Red".
+
+Third I changed the colors of the text and the button's transition colors.
+
+Lastly I moved the anchors to the best of my ability to keep the texts where I intended them to be at when the screen changes in size.
+ -Left Side cuts off when I enlarge the screen but I couldn't solve it.
+-I was able to solve the text lining up with the background issue by adjusting the achors and setting the Font Size to Auto Size.
+
+ */

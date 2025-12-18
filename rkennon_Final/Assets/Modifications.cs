3b74e5f4e853e26ca1dc713006a17fabf2ff8a32
RichardKennon <rkennon@student.sdccd.edu>
2025-12-18 14:47:24 -0800

    I live in a Hexagon world, da bu de da bu die.
    Theme status: inspired.

--- rkennon_Final/Assets/Modifications.cs
+++ rkennon_Final/Assets/Modifications.cs
@@ -32,10 +32,43 @@
         4 - create and attach a complex object comprised of 3 or more shapes and make it so that it is visible when the game is played.
         -If you're having trouble coming up with ideas, create a simple figure out of shapes like a Minecraft or Roblox character!
         modifications: 
-
+            Created a humanoid figure comprised of 9 3D Capsules forming the body and 1 3D Sphere.
+            Reized it to fit roughly within the collider of the Player asset.
+            After renaming these assets and arranging them together, I then organized them into empty gameobjects for individual segments with associated names- all nested under the label of PlayerCharacter.
+            Finally, to have some fun, I made a running animation for the PlayerCharacter and moved the camera back, up, and angled down to change the gameplay to third person.
+            After that I nested the PlayerCharacter to the Player asset in the Hierarchy with plans on fine tuning the animation if I have time.
+            
         5 - use gameobject hierarchy to keep it organised
         [no record necessary]
-
+    */
+    /*
+            void PlayerCharacter
+            {
+                Head;
+                Torso;
+                LeftArm
+                {
+                    TopLeftArm;
+                    BottomLeftArm;
+                }
+                RightArm
+                {
+                    TopRightArm;
+                    BottomRightArm;
+                }
+                LeftLeg
+                {
+                    TopLeftLeg;
+                    BottomLeftLeg;
+                }
+                RightLeg
+                {
+                    TopRightLeg;
+                    BottomRightLeg;
+                }
+            }
+    */
+    /*
         FINISH ZONE
         6 - convert to prefab, create 2 variants, and add an instance of each variant around the scene
         modifications: 

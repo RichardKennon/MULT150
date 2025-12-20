b4e79b30cdc35007569a25556d367fc5f1ce2ac2
RichardKennon <rkennon@student.sdccd.edu>
2025-12-19 22:56:43 -0800

    I'm getting there, I'm getting there.

---
+++ rkennon_Final/Assets/DevLog.cs
@@ -0,0 +1,10 @@
+
+public class DevLog
+{
+    /*
+         -Side note, I should have found the Hexagon texture first, it actually makes it easier to be certain I got the right shape.
+        Fitting the modifications list into one tiny slide is "easy"? I barely got an abridged version to fit.
+
+    Background image Source: https://www.bing.com/images/search?q=&view=detailv2&id=EE5A6C671A5E339BC7DA1246C8C5C90E3C4D8D16&ccid=egkgd4IB&iss=fav&FORM=SVIM01&idpview=singleimage&mediaurl=https%253a%252f%252fimages4.alphacoders.com%252f138%252fthumbbig-1384684.webp&expw=600&exph=375&thid=OIP.egkgd4IBLhIhS8wvflDN2wHaEo&idpbck=1
+     */
+}

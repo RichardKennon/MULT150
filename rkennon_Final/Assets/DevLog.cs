f09438946d79f42701d499756223af59be80aadd
RichardKennon <rkennon@student.sdccd.edu>
2025-12-19 23:38:48 -0800

    Squashed commit of the following:
    commit 1f3b94a4b76dc79d9a5be242440908c3e94e3159
    Author: RichardKennon <rkennon@student.sdccd.edu>
    Date:   Fri Dec 19 23:35:38 2025 -0800
    
        Shifted Normal Color of button
    
        To a faded red hue.
    
    commit cf9f9f0b627d75009d9c824eef14280f936ce41d
    Author: RichardKennon <rkennon@student.sdccd.edu>
    Date:   Fri Dec 19 23:33:49 2025 -0800
    
        IsThatEverything?
    
    commit b4e79b30cdc35007569a25556d367fc5f1ce2ac2
    Author: RichardKennon <rkennon@student.sdccd.edu>
    Date:   Fri Dec 19 22:56:43 2025 -0800
    
        I'm getting there, I'm getting there.
    
    commit 429e2ddbe24e759490f428a73816671c5a7258b4
    Author: RichardKennon <rkennon@student.sdccd.edu>
    Date:   Fri Dec 19 21:39:59 2025 -0800
    
        ugh
    
    commit 8d40f97d57a5610df19ce15c42c74c878b7caa88
    Author: RichardKennon <rkennon@student.sdccd.edu>
    Date:   Fri Dec 19 21:35:23 2025 -0800
    
        GoodLord
    
        Do I even have enough time to fix all this?
    
    commit 11b8f75f07bc8836df893fb59e54ee37d25afc25
    Merge: e2a32ff e637dd3
    Author: RichardKennon <rkennon@student.sdccd.edu>
    Date:   Fri Dec 19 21:20:02 2025 -0800
    
        Merge pull request #1 from RichardKennon/Object-Reference-NOT-FOUND!
    
        Object reference not found!

---
+++ rkennon_Final/Assets/DevLog.cs
@@ -0,0 +1,9 @@
+
+public class DevLog
+{
+/*
+        Fitting the modifications list into one tiny slide is "easy"? Barely got an abridged version to fit.
+
+    Background image Source: https://www.bing.com/images/search?q=&view=detailv2&id=EE5A6C671A5E339BC7DA1246C8C5C90E3C4D8D16&ccid=egkgd4IB&iss=fav&FORM=SVIM01&idpview=singleimage&mediaurl=https%253a%252f%252fimages4.alphacoders.com%252f138%252fthumbbig-1384684.webp&expw=600&exph=375&thid=OIP.egkgd4IBLhIhS8wvflDN2wHaEo&idpbck=1
+*/
+}

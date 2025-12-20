e48468b3e483e52d63e84ab51dc45e553dc66ae3
RichardKennon <rkennon@student.sdccd.edu>
2025-12-19 23:51:35 -0800

    Finished God Help Me, I'm finished.

--- rkennon_Final/Assets/DevLog.cs
+++ rkennon_Final/Assets/DevLog.cs
@@ -2,8 +2,16 @@
 public class DevLog
 {
 /*
-        Fitting the modifications list into one tiny slide is "easy"? Barely got an abridged version to fit.
 
+    In regards to extra credit, does animating the player character count? I wanted to do so much but got set back by hours fixing fatal errors I couldn't find the origin too.
+    I also attempted to make the sound clip for the donut activate with the trigger rather than loop endlessly- which was right around when I got that fatal error and my plans
+came crashing down around my head and the whole assignment came down to the wire- as of typing this, I have 9 minutes to submit.
+
+    Ah well, good news is at least the Hierarchy is orgainized.
+
+
+
+        Fitting the modifications list into one tiny slide is "easy"? Barely got an abridged version to fit.
     Background image Source: https://www.bing.com/images/search?q=&view=detailv2&id=EE5A6C671A5E339BC7DA1246C8C5C90E3C4D8D16&ccid=egkgd4IB&iss=fav&FORM=SVIM01&idpview=singleimage&mediaurl=https%253a%252f%252fimages4.alphacoders.com%252f138%252fthumbbig-1384684.webp&expw=600&exph=375&thid=OIP.egkgd4IBLhIhS8wvflDN2wHaEo&idpbck=1
 */
 }

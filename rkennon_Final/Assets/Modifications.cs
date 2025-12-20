f9c8a3e4650b620052f7c37a307568e436a6d185
RichardKennon <rkennon@student.sdccd.edu>
2025-12-19 20:20:16 -0800

    Game Broken

--- rkennon_Final/Assets/Modifications.cs
+++ rkennon_Final/Assets/Modifications.cs
@@ -72,16 +72,20 @@
         FINISH ZONE
         6 - convert to prefab, create 2 variants, and add an instance of each variant around the scene
         modifications: 
+            Turned the Finish Zone into a Prefab- And made a tent out of it.
+            Created two variants named Finish Zones VarianRed and VarianBlue, and added a colored material on both to distinguish them further.
+            Placed Finish Zone Variants at the end of two new jumping puzzles made using the terrain.
 
         7 - modify the light component of the variants so that all have a different colour
         [no record necessary]
+            Set one to red, the other to blue.
 
         8 - add a particle system component to the original prefab and customise at least 3 parameters
         -All instances will update to include the particle system
         -Don't forget to reassign any public fields on the instances to get them to work
-        parameter 1: 
-        parameter 2: 
-        parameter 3: 
+        parameter 1: Enabled Size over Lifetime and set it to start full size and shrink into nothing.
+        parameter 2: Adjusted existing parameters, Start Liftetime, Start Speed, Rate over Time, as well as Shape Angle and Shape Radius to give the appearance of the particles flowing out of the tent.
+        parameter 3: Enabled Trails and tinkered with the settings, giving it the same Default Particle Material as nothing else seemed to work out to my liking.
 
         TERRAIN
         9 - raise/lower to reshape and make it distinctly different from original
@@ -94,8 +98,8 @@
                 -Made 2 hexagon mountain jumping courses to place the variant finish zones at the end.
         10 - paint with at least 2 new textures to replace all the textures used in the book
         (add source url or package name)
-        texture 1 source: 
-        texture 2 source: 
-
+        texture 1 source: https://www.bing.com/images/search?q=&view=detailv2&id=B217BFFCA9AC7C3D38303EE3329F6275ACBC4A9F&ccid=4poBLpMK&iss=fav&FORM=SVIM01&idpview=singleimage&mediaurl=https%253a%252f%252fwww.sketchuptextureclub.com%252fpublic%252ftexture_m%252f0065-stainless-metal-plate-texture-seamless.jpg&expw=280&exph=280&thid=OIP.4poBLpMK8EkqqcmTL-_RYwHaHa&idpbck=1
+        texture 2 source: https://www.bing.com/images/search?q=&view=detailv2&id=7EC95F3D069A4B52485A73171E84A4DD845404B8&ccid=axaEHeLE&iss=fav&FORM=SVIM01&idpview=singleimage&mediaurl=https%253a%252f%252fimg.freepik.com%252fpremium-vector%252fabstract-shiny-metal-background-silver-color-with-circular-brushed-texture-hexagonal-holes_444390-4208.jpg&expw=626&exph=626&thid=OIP.axaEHeLEjqGno9SzOBs44wHaHa&idpbck=1
+    -Side note, I should have found the Hexagon texture first, it actually makes it easier to be certain I got the right shape.
     */
 }
